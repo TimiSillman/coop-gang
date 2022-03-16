c16ba401317efebe99b7b15923f2998698aa49e5
Timi Sillman <sillmantimi@gmail.com>
2022-03-16 19:05:43 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DiscordBotModule } from './discord-bot/discord-bot.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), DiscordBotModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
